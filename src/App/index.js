bef526c61123d7224ae49b08f0b14f4f7943732d
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-04-27 18:21:02 -0500

    added new files

--- src/App/index.js
+++ src/App/index.js
@@ -1,34 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import './App.scss';
-import StudentForm from '../StudentForm';
-import { getStudents } from '../helpers/data/studetnData';
-import StudentCard from '../Components/StudentCard';
+import React from 'react';
 
 function App() {
-  const [students, setStudents] = useState([]);
-
-  useEffect(() => {
-    getStudents().then((response) => setStudents(response));
-  }, []);
-
   return (
-    <div className='App'>
-      <StudentForm
-      formTitle= 'Form Name'
-      setStudents={setStudents}
-      />
-      <hr/>
-      {students.map((studentInfo) => (
-       <StudentCard
-        key={studentInfo.firebaseKey}
-        firebaseKey={studentInfo.firebaseKey}
-        name={studentInfo.name}
-        teacher={studentInfo.teacher}
-        grade={Number(studentInfo.grade)}
-        setStudents={setStudents}
-       />
-      ))}
-    </div>
+    <h1>Hello World!</h1>
   );
 }
 
